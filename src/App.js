44fc7fcdead96eae63db7fd5cc7a15a20f1a67de
sofiaIG <user@users-MacBook-Pro.local>
2022-05-31 14:13:41 +0100

    initialising project. Have tested if the app can communicate with back-end.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+  const [posts, setPosts] = useState("")
+
+  useEffect(()=>{
+    getPosts();
+  })
+
+  const getPosts = function(){
+    fetch("http://localhost:8080/posts")
+    .then(res =>res.json())
+    .then(posts =>setPosts(posts))
+  }
+
+
+
+  // const printPosts = posts.map((post) =>{
+  //   return post;
+  // })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log(posts)}
+      {/* {console.log(printPosts())} */}
+
+    
     </div>
   );
 }
